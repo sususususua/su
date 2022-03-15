4e15f8b630533515f3a00b61ced41433cec7ea1f
HelloNoa <ohyoungwoo824@gmail.com>
2022-03-16 01:39:39 +0900

    feat : carousel 이게 맞을까요

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.onload = ()=>{
-    console.log("매롱");
+    document.querySelectorAll('ul.carousel li').forEach((e, idx)=>{
+        const ul = document.querySelector('ul.carousel_list');
+        const li = document.createElement('li');
+        li.textContent= idx;
+        ul.appendChild(li);
+    });
+    document.querySelectorAll('ul.carousel_list li').forEach((e, idx)=>{
+        e.addEventListener('click',el=>{
+            document.querySelector('ul.carousel').style.marginLeft = `-${idx*100}%`;
+        });
+    });
 }
